58afbdea85d88e9f7c0700c67f5664ec37ab3174
chioreandan <chiorean.dan12@gmail.com>
2018-06-11 11:50:38 +0300

    Add and view images for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,33 +1,26 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
-  # GET /products
-  # GET /products.json
   def index
     @products = Product.all
   end
 
-  # GET /products/1
-  # GET /products/1.json
   def show
   end
 
-  # GET /products/new
   def new
     @product = Product.new
   end
 
-  # GET /products/1/edit
   def edit
   end
 
-  # POST /products
-  # POST /products.json
   def create
     @product = Product.new(product_params)
 
     respond_to do |format|
       if @product.save
+        @product.image.attach(params[:product][:image]) if params[:product][:image].present?
         format.html { redirect_to @product, notice: 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
       else
@@ -35,11 +28,13 @@ class ProductsController < ApplicationController
         format.json { render json: @product.errors, status: :unprocessable_entity }
       end
     end
+
   end
 
-  # PATCH/PUT /products/1
-  # PATCH/PUT /products/1.json
   def update
+    @product.assign_attributes(product_params)
+    @product.image.attach(params[:product][:image])
+
     respond_to do |format|
       if @product.update(product_params)
         format.html { redirect_to @product, notice: 'Product was successfully updated.' }
@@ -51,8 +46,6 @@ class ProductsController < ApplicationController
     end
   end
 
-  # DELETE /products/1
-  # DELETE /products/1.json
   def destroy
     @product.destroy
     respond_to do |format|
@@ -62,13 +55,11 @@ class ProductsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :category, :price)
+      params.require(:product).permit(:name, :category, :price, :image)
     end
 end
