e2634d0ec667a5c4b1be681af670b0e7d70b5dfa
chioreandan <chiorean.dan12@gmail.com>
2018-06-28 17:00:20 +0300

    Add log-in, log-out and admin authorization

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -1,4 +1,5 @@
 class LandingPagesController < ApplicationController
+  skip_before_action :authorize_admin, only: [:home]
 
   def home
     @products = Product.all
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authorize_admin, only: [:show]
 
   def index
     @products = Product.paginate(page: params[:page], per_page: 4)
