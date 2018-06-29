f36e53ae4c785f7ec481fa5df2bc22c7ca21a917
chioreandan <chiorean.dan12@gmail.com>
2018-06-29 15:27:09 +0300

    Db schema change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_29_095914) do
+ActiveRecord::Schema.define(version: 2018_06_29_111927) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2018_06_29_095914) do
     t.datetime "updated_at", null: false
     t.integer "order_id"
     t.integer "product_id"
+    t.integer "product_count"
     t.index ["order_id"], name: "index_order_details_on_order_id"
     t.index ["product_id"], name: "index_order_details_on_product_id"
   end
