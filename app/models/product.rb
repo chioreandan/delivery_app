98e6a083766ce139cb9f7629d278593698728b50
chioreandan <chiorean.dan12@gmail.com>
2018-06-29 11:06:56 +0300

    Add product validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
+  validates :category, presence: true, length: {maximum: 20}
   has_one_attached :image
 end
